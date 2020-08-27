7d7c22a173528661cbee6f949449e8c00b2ff67d
Devnii-ksnwt <65270284+Devnii-ksnwt@users.noreply.github.com>
2020-08-27 10:28:22 +0700

    Add files via upload

---
+++ Contoh.php
@@ -0,0 +1,28 @@
+<?php 
+
+	//Devni Kusnia Wati
+	//RPL XI-1
+    //Cicurug 5
+
+	class Pakaian {
+		public $warna,
+			   $ukuran,
+			   $bahan,
+			   $harga;
+
+		public function getbuy(){
+			return "$this->warna,$this->ukuran,
+			        $this->bahan,$this->harga";
+		}
+	}
+
+	$baju = new Pakaian();
+	$baju->warna = " Hijau";
+	$baju->ukuran = " XL";
+	$baju->bahan = " Katun";
+	$baju->harga = 75000;
+
+
+	echo "Baju : ". $baju->getbuy();
+
+?>
